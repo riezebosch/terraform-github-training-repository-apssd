ab1344456a02feb44f3fa86cf2daf208ac224b15
Manuel Riezebosch <mriezebosch@gmail.com>
2024-02-19 14:42:57 +0100

    chore: update github provider

--- modules/team/providers.tf
+++ modules/team/providers.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.0.0"
+    }
+
     value = {
       source  = "pseudo-dynamic/value"
       version = "0.5.1"
@@ -11,4 +16,5 @@ terraform {
     }
   }
 }
+
 
