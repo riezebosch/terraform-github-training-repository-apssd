e89c500bbc389ba7653f13607019b14709ab0f06
Manuel Riezebosch <mriezebosch@gmail.com>
2024-02-19 14:43:19 +0100

    fix(stack): add nodejs and python to validation

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "teams" {
     stack = string
   }))
   validation {
-    condition     = alltrue([for team in var.teams : contains(["dotnet", "java", "go", "php"], team.stack)])
+    condition     = alltrue([for team in var.teams : contains(["dotnet", "java", "go", "php", "nodejs", "python"], team.stack)])
     error_message = "stack must be one of: dotnet"
   }
 }
